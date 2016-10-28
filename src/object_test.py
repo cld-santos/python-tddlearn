9ca13177e560ebe4cd4565bf54430b19f10d077f
Claudio Santos <contato@simplologia.com.br>
2016-10-28 18:21:25 -0200

    declarando variaveis como no javascript e quebrando o escopo

--- src/object_test.py
+++ src/object_test.py
@@ -3,6 +3,15 @@ from Montadora.Carro import Automovel
 from Montadora.TipoCarro import Cabritao
 from Montadora.TipoCarro import Esportivo
 
+
+class Engradado:
+    _brejas = []
+
+    def mais_breja(self,breja):
+        self._brejas.append(breja)
+    def brejas(self):
+        return self._brejas
+
 class object_test(unittest.TestCase):
     def test_acessa_variavel_privada(self):
         gol = Automovel("wolksvagen","cabritão")
@@ -15,6 +24,16 @@ class object_test(unittest.TestCase):
         gol = Cabritao("Wolksvagen")
         civic = Esportivo("Honda")
         self.assertTrue(civic.tipo=="Esportivo")
+
+
+    def test_escopo_compartilhado_variavel_sem_self(self):
+        _kaiser = Engradado()
+        _kaiser.mais_breja('xixi de galinha')
+        _kaiser.mais_breja('suor de gordo peludo')
+
+        _amstel = Engradado()
+
+        self.assertTrue(_kaiser.brejas() == _amstel.brejas())
 
 if __name__ == "__main__":
     unittest.main()
