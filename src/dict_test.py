da31177b963e4dd1c5c2523535d294c17174a9fe
Claudio Santos <contato@simplologia.com.br>
2016-10-27 22:06:38 -0200

    uso de módulos

--- src/dict_test.py
+++ src/dict_test.py
@@ -18,5 +18,20 @@ class test_dict(unittest.TestCase):
             self.fail("should be got error")
 
 
+    def test_walk_through_tree(self):
+        separator = ','
+
+        def walk_through(tree):
+            if len(tree['sun'])>0:
+                return tree['name'] + separator + walk_through(tree['sun'])
+            else:
+                return tree['name'] 
+
+        family_sheep = {'name':'grangranpa', 'sun':{'name':'granpa','sun':{'name':'shaun', 'sun':{'name':"shaun's sun", 'sun':{}}}}}
+        
+        self.assertTrue(walk_through(family_sheep)== "grangranpa,granpa,shaun,shaun's sun")
+
+
+
 if __name__ == "__main__":
     unittest.main()
