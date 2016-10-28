7a9d2e3756a6ca0639bb1a80577caa065bda9f45
Claudio Santos <contato@simplologia.com.br>
2016-10-28 18:22:19 -0200

    implementando o off

--- src/util/EventOriented.py
+++ src/util/EventOriented.py
@@ -1,11 +1,16 @@
 class Event:
-    _events = {}
+    def __init__(self):
+        self._events = {}
     
     def on(self, name, action):
         if name not in self._events.keys():
             self._events[name] = []
         self._events[name].append(action)
 
-    def emit(self, name):
+    def off(self, name, action):
+        self._events[name].remove(action)
+    
+
+    def emit(self, name, paramns):
         for _item in self._events[name]:
-            _item()
+            _item(paramns)
