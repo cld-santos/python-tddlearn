7f38c42df6111767c92fede8f2d917954e91fe4e
Claudio Santos <contato@simplologia.com.br>
2016-10-31 22:38:21 -0200

    testing django templates language

--- src/template_test.py
+++ src/template_test.py
@@ -1,13 +1,21 @@
+import os
 import unittest
 from django.template import Template, Context, Engine
 from django.conf import settings
 
+
 class template_test(unittest.TestCase):
-    def test_render_a_template(self):
+    def test_render_a_variable_template(self):
         _engine = Engine() 
         _template = _engine.from_string("Simple replacement {{ target }}.")
         _context = Context({"target":"Claudio Santos"})
         self.assertTrue(_template.render(_context) == "Simple replacement Claudio Santos.")
+
+    def test_render_a_file_template(self):
+        _engine = Engine(dirs='templates/')
+        _template = _engine.get_template('/home/ubuntu/src/template/base.html')
+        _context = Context({'name': 'Claudio Santos', 'biograph':'programmer ancious to learn python'})
+        self.assertTrue(_template.render(_context)=="<html><body>Meu Nome é Claudio Santos e sou programmer ancious to learn python</body></html>\n")
 
 if __name__ == "__main__":
     import django
