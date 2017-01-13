57f2ed120b2efc63c658cc5e6166ad3b61f73587
Claudio Santos <contato@simplologia.com.br>
2017-01-12 22:48:46 -0200

    configure ROOT_URLCONF

--- src/settings/test.py
+++ src/settings/test.py
@@ -1,8 +1,6 @@
 import os
 
+DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'local'
-
-# Test runner with no database creation
-DEBUG = True
-
+ROOT_URLCONF = 'web.urls'
