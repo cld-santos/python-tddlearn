0dafc0c69a9536e238adc80c096550e314bae3dd
Claudio Santos <contato@simplologia.com.br>
2017-01-12 23:50:16 -0200

    parametrized url test

--- src/web/urls.py
+++ src/web/urls.py
@@ -2,14 +2,22 @@
 from django.conf.urls import url
 from django.http import HttpResponse
 
-urlpatterns = []
+urlpatterns = None
 
 
 class Urls(object):
 
+    def setup_urls(self):
+        global urlpatterns
+        # urlpatterns.append(url(r'^(?P<categoria>[a-z]+)/(?P<post>[a-z]+)/$', self.parametrized_url))
+        # urlpatterns.append(url(r'^$', self.index))
+        urlpatterns = (
+            url(r'^(?P<categoria>[a-z]+)/(?P<post>[a-z]+)/$', self.parametrized_url, name='parametrized_url'),
+            url(r'^$', self.index, name='index'),
+        )
+
     def index(self, request):
         return HttpResponse("Hello World!")
 
-    def setup_urls(self):
-        global urlpatterns
-        urlpatterns.append(url(r'^$', self.index))
+    def parametrized_url(self, request, categoria, post):
+        return HttpResponse("Post {0} da {1}.".format(post, categoria))
